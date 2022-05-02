c63399210e28da3f15ea546126fdd22b43cc4d4f
landbu <95740885+landbu@users.noreply.github.com>
2022-05-02 19:59:48 +0200

    Update Minecraftsweeper.py

--- Minecraftsweeper.py
+++ Minecraftsweeper.py
@@ -3,13 +3,14 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.title("Mine Sweeper")
+root.title("Start menu")
 map = []
 map_list = []
 
 
 class Tile:
-    def __init__(self, lava_bool: bool, x_cord, y_cord, nearby_lava, coverd_bool: bool, nears, state, button, prime_nears):
+    def __init__(self, lava_bool: bool, x_cord, y_cord, nearby_lava, coverd_bool: bool, nears, state, button,
+                 prime_nears):
         self.lava_bool = lava_bool
         self.x_cord = x_cord
         self.y_cord = y_cord
@@ -19,7 +20,6 @@ class Tile:
         self.state = state
         self.button = button
         self.prime_nears = prime_nears
-
 
     def get_lava_bool(self):
         return self.lava_bool
@@ -53,7 +53,8 @@ class Tile:
 
     def clicked(self):
         self.coverd_bool = False
-        clicked(self) # Detta ser väldigt underligt ut, jag vet, men det sätt jag ville använda fungerade inte, detta löste problemet
+        clicked(
+            self)  # Detta ser väldigt underligt ut, jag vet, men det sätt jag ville använda fungerade inte, detta löste problemet
 
 
 def map_creator(width, height):
@@ -62,20 +63,20 @@ def map_creator(width, height):
     for row in range(height):
         row_list = []
         for column in range(width):
-            lava_bool = random.choice([True, False, False, False, False]) #Justera sannolikheten senare
+            lava_bool = random.choice([True, False, False, False, False])  # Justera sannolikheten senare
 
             if row == 0:
                 if column == 0:
                     state = "top_left"
-                elif column == width-1:
+                elif column == width - 1:
                     state = "top_right"
                 else:
                     state = "top"
 
-            elif row == height-1:
+            elif row == height - 1:
                 if column == 0:
                     state = "bot_left"
-                elif column == width-1:
+                elif column == width - 1:
                     state = "bot_right"
                 else:
                     state = "bot"
@@ -83,13 +84,14 @@ def map_creator(width, height):
             elif column == 0:
                 state = "left"
 
-            elif column == width-1:
+            elif column == width - 1:
                 state = "right"
 
             else:
                 state = "normal"
 
-            row_list.append(Tile(lava_bool, x, y, "uncalculated", True, "uncalculated", state, "uncalculated", "uncalculated"))
+            row_list.append(
+                Tile(lava_bool, x, y, "uncalculated", True, "uncalculated", state, "uncalculated", "uncalculated"))
             x += 1
         map.append(row_list)
         y += 1
@@ -111,19 +113,25 @@ def calculate_nears():
             elif tile_state == "bot_right":
                 nears = [map[-2][-2], map[-2][-1], map[-1][-2]]
             elif tile_state == "top":
-                nears = [map[0][x_index-1], map[0][x_index+1], map[1][x_index-1], map[1][x_index], map[1][x_index+1]]
+                nears = [map[0][x_index - 1], map[0][x_index + 1], map[1][x_index - 1], map[1][x_index],
+                         map[1][x_index + 1]]
             elif tile_state == "bot":
-                nears = [map[y_index-1][x_index-1], map[y_index-1][x_index], map[y_index-1][x_index+1], map[y_index][x_index-1], map[y_index][x_index+1]]
+                nears = [map[y_index - 1][x_index - 1], map[y_index - 1][x_index], map[y_index - 1][x_index + 1],
+                         map[y_index][x_index - 1], map[y_index][x_index + 1]]
             elif tile_state == "left":
-                nears = [map[y_index-1][x_index], map[y_index-1][x_index+1], map[y_index][x_index+1], map[y_index+1][x_index], map[y_index+1][x_index+1]]
+                nears = [map[y_index - 1][x_index], map[y_index - 1][x_index + 1], map[y_index][x_index + 1],
+                         map[y_index + 1][x_index], map[y_index + 1][x_index + 1]]
             elif tile_state == "right":
-                nears = [map[y_index-1][x_index-1], map[y_index-1][x_index], map[y_index][x_index-1], map[y_index+1][x_index-1],map[y_index+1][x_index]]
+                nears = [map[y_index - 1][x_index - 1], map[y_index - 1][x_index], map[y_index][x_index - 1],
+                         map[y_index + 1][x_index - 1], map[y_index + 1][x_index]]
             elif tile_state == "normal":
-                nears = [map[y_index-1][x_index-1], map[y_index-1][x_index], map[y_index-1][x_index+1], map[y_index][x_index-1], map[y_index][x_index+1], map[y_index+1][x_index-1], map[y_index+1][x_index], map[y_index+1][x_index+1]]
+                nears = [map[y_index - 1][x_index - 1], map[y_index - 1][x_index], map[y_index - 1][x_index + 1],
+                         map[y_index][x_index - 1], map[y_index][x_index + 1], map[y_index + 1][x_index - 1],
+                         map[y_index + 1][x_index], map[y_index + 1][x_index + 1]]
             tile.set_nears(nears)
             x_index += 1
         x_index = 0
-        y_index +=1
+        y_index += 1
 
 
 def calculate_prime_nears():
@@ -139,9 +147,11 @@ def calculate_prime_nears():
             elif state == "left":
                 tile.set_prime_nears([tile.get_nears()[0], tile.get_nears()[2], tile.get_nears()[3]])
             elif state == "normal":
-                tile.set_prime_nears([tile.get_nears()[1], tile.get_nears()[3], tile.get_nears()[4], tile.get_nears()[6]])
+                tile.set_prime_nears(
+                    [tile.get_nears()[1], tile.get_nears()[3], tile.get_nears()[4], tile.get_nears()[6]])
             elif state == "right":
-                tile.set_prime_nears([tile.get_nears()[1], tile.get_nears()[2], tile.get_nears()[0], tile.get_nears()[4]])
+                tile.set_prime_nears(
+                    [tile.get_nears()[1], tile.get_nears()[2], tile.get_nears()[0], tile.get_nears()[4]])
             elif state == "bot_left":
                 tile.set_prime_nears([tile.get_nears()[0], tile.get_nears()[2]])
             elif state == "bot_right":
@@ -160,11 +170,11 @@ def calculate_nearby_lava():
 
 
 def big_clear(original_tile):
-    #funktionen ansvarar för att göra de stora clearsen
+    # funktionen ansvarar för att göra de stora clearsen
 
-    big_clear_list = [] # Lista sammankoppalade tiles utan nrliggandelava
+    big_clear_list = []  # Lista sammankoppalade tiles utan nrliggandelava
     latest_list = []
-    big_display_list = [] # Lista av alla tiles som ska synas efter processen är klar som har närliggande lava
+    big_display_list = []  # Lista av alla tiles som ska synas efter processen är klar som har närliggande lava
 
     for near in original_tile.get_nears():
         if near.get_nearby_lava() == 0:
@@ -174,7 +184,7 @@ def big_clear(original_tile):
             big_display_list.append(near)
 
     while True:
-        if len(latest_list) == 0: #Om det inte finns något mer all cleara, avbryt loopen
+        if len(latest_list) == 0:  # Om det inte finns något mer all cleara, avbryt loopen
             break
         temp_list = latest_list
         latest_list = []
@@ -185,7 +195,6 @@ def big_clear(original_tile):
                     latest_list.append(near)
                 elif near not in big_clear_list:
                     big_display_list.append(near)
-
 
     for tile in big_clear_list:
         tile.set_button(Label(root, text="0"))
@@ -200,11 +209,9 @@ def game_over():
             if tile.get_lava_bool():
                 tile.set_button(Label(root, text="B"))
             else:
-                tile.set_button(Label(root, text=tile.get_nearby_lava())) # fixa sedan så man kan se vilka man lyckades få och inte
+                tile.set_button(
+                    Label(root, text=tile.get_nearby_lava()))  # fixa sedan så man kan se vilka man lyckades få och inte
     messagebox.showerror("Game Over", "You lost :(\n Click ok to display the uncoverd map")
-
-
-
 
 
 def clicked(tile):
@@ -215,8 +222,10 @@ def clicked(tile):
         game_over()
         tile.set_button(Label(root, text="B", bg="red"))
     else:
-        if tile.get_nearby_lava() != 0: tile.set_button(Label(root, text=tile.get_nearby_lava())) #displayar antal närliggande lava
-        else: big_clear(tile)
+        if tile.get_nearby_lava() != 0:
+            tile.set_button(Label(root, text=tile.get_nearby_lava()))  # displayar antal närliggande lava
+        else:
+            big_clear(tile)
     play()
 
 
@@ -230,7 +239,6 @@ def map_render(width):
 
 
 def play():
-
     x = 0
     y = 0
     for tile in map_list:
@@ -241,19 +249,52 @@ def play():
             x = 0
 
 
-def main():
-    print("Welcome to main!")
-    width = int(input("Width of map: "))
-    height = int(input("height of map: "))
+def menu_confirm(width, height):
+    try:
+        width = int(width)
+        height = int(height)
+        if 34 > width > 1 and 21 > height > 1:  # Säkerställer användaren skriver in rimliga nummer
+            global root
+            root.destroy()  # Följande 3 lines är till för att reseta root fönstret
+            root = Tk()
+            root.title("Mine Sweeper")
+            main(width, height)
+        elif width > 33:
+            messagebox.showerror("Error", "The (1) witdh you have enterd is\ntoo big, use a witdh smaller than 34")
+        elif height > 20:
+            messagebox.showerror("Error", "The (2) height you have enterd is\ntoo big, use a height smaller than 21")
+        elif width < 2 or height < 2:
+            messagebox.showerror("Error", "The minimum map size is 2X2\nenter bigger numbers")
+        else:
+            messagebox.showerror("Error", "Unecpected error")
+
+    except:
+        messagebox.showerror("Error", "Please enter intergers")
+
+
+def menu():
+    confirm_button = Button(root, text="confirm", command=lambda: menu_confirm(width_input.get(), height_input.get()))
+    width_input = Entry(root, text="test")
+    height_input = Entry(root)
+    welcome_box = Label(root, text="Welcome!", font=("Arial", 25))
+    welcome_text = Label(root, text="Please enter the 1. witdh and \n2. height of your desired map")
+
+    welcome_text.grid(row=0, column=1)
+    welcome_box.grid(row=0, column=0)
+    width_input.grid(row=1, column=0)
+    height_input.grid(row=1, column=1)
+    confirm_button.grid(row=1, column=2)
+
+
+def main(width, height):
     map_creator(width, height)
     calculate_nears()
     calculate_nearby_lava()
-    #calculate_prime_nears() jag tror faktiskt inte detta behövs
+    # calculate_prime_nears() jag tror faktiskt inte detta behövs
     map_render(width)
     play()
 
-    root.mainloop()
-
 
 if __name__ == "__main__":
-    main()
+    menu()
+    root.mainloop()
